4cf6d26977314c854ac78a764a291a177a389688
serraguti <pacoserranox@gmail.com>
2023-06-21 13:43:48 +0200

    Add project files.

---
+++ NugetSeriesPersonajes/Personaje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetSeriesPersonajes
+{
+    [Table("PERSONAJES")]
+    public class Personaje
+    {
+        [Key]
+        [Column("IDPERSONAJE")]
+        public int IdPersonaje { get; set; }
+        [Column("PERSONAJE")]
+        public string Nombre { get; set; }
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+        [Column("IDSERIE")]
+        public int IdSerie { get; set; }
+    }
+}
